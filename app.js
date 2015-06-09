81df82e851a4aa29a252ef1645251a3fbead2f32
Jhorlin <jhorlin@gmail.com>
2015-06-09 03:09:31 -0400

    added initial heatmap

--- app.js
+++ app.js
@@ -1,37 +1,63 @@
 /**
  * Created by jhorlin.dearmas on 6/8/2015.
  */
-(function(){
+(function () {
     'use strict';
 
     var express = require('express'),
         env = require('node-env-file'),
         path = require('path'),
-        httpProxy = require('http-proxy'),
-        proxy = httpProxy.createProxyServer({}),
-        harmon = require('harmon'),
-        enrich = require('./lib/enrich'),
-        enrichmentHandler = harmon([], [enrich]),
+        request = require('request'),//.defaults({proxy:'http://localhost:8888'}),
+        trumpet = require('trumpet'),
+        extend = require('extend'),
+        through = require('through2'),
+        url = require('url'),
+        fs = require('fs'),
+        noop = function (chunk, enc, cb) {
+            cb(null, chunk);
+        },
         app = express();
 
     env(path.join(__dirname, '.env'));
 
     var port = process.env.PORT,
-        target = process.env.TARGET;
+        target = process.env.TARGET,
+        host = url.parse(target).host;
 
-    app.listen(port, function(){
+    app.listen(port, function () {
         console.log('listening on port %s', port);
     });
 
     app.use('/components', express.static(path.join(__dirname, 'components')));
     app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 
-    app.use(enrichmentHandler);
+    app.use(function (req, res) {
+        var tr = trumpet();
+        tr.pipe(res);
+        res.removeHeader('Content-Length');
+        var headStream = tr.select('head').createStream(),
+            bodyStream = tr.select('body').createStream(),
+            heatMapComponentStream = fs.createReadStream('components/heatmap/index.html');
 
-    app.use(function (req, res, next) {
-        proxy.web(req, res, {target: target}, function(err){
-            console.log(err.message);
-        })
+        headStream.pipe(through(noop, function (cb) {
+            var self = this;
+            heatMapComponentStream.on('data', this.push.bind(self));
+            heatMapComponentStream.on('end', cb);
+            heatMapComponentStream.on('error', cb);
+        })).pipe(headStream);
+
+        bodyStream.pipe(through(noop, function (cb) {
+            this.push('<div id="heatMapContainer"></div>');
+            cb();
+        })).pipe(bodyStream);
+
+        request({
+            method: req.method,
+            uri: target + req.originalUrl,
+            headers: extend({}, req.headers, {
+                host: host
+            })
+        }).pipe(tr);
     });
 
 }())
--- components/heatmap/jsx/heatmap.js
+++ components/heatmap/jsx/heatmap.js
@@ -1,13 +1,72 @@
 /**
  * Created by jhorlin.dearmas on 6/8/2015.
  */
-(function(React){
-    var heatMap = React.createClass({
-        render:function(){
-            return(<div className="heatMap">hello</div>);
+(function (window, React) {
+    var HeatMap = React.createClass({
+        handleResize: function () {
+            var self = this;
+            if (this.timeout) {
+                window.clearTimeout(this.timeout);
+                this.timeout = null;
+            }
+            this.timeout = window.setTimeout(function () {
+                self.setState({
+                    height: document.body.clientHeight,
+                    width: document.body.clientWidth,
+                    interactions: self.state.interactions
+                });
+            }, 3000)
+
+        },
+        handleInteraction: function (e) {
+            this.state.interactions.push(e);
+            this.setState({
+                height: this.state.clientHeight,
+                width: this.state.clientWidth,
+                interactions: this.state.interactions
+            })
+        },
+        componentDidMount: function () {
+            window.addEventListener('resize', this.handleResize);
+            window.addEventListener('click', this.handleInteraction);
+        },
+
+        componentWillUnmount: function () {
+            window.removeEventListener('resize', this.handleResize);
+            window.removeEventListener('click', this.handleInteraction);
+        },
+        getInitialState: function () {
+            return {
+                height: document.body.clientHeight,
+                width: document.body.clientWidth,
+                interactions: []
+            };
+        },
+        render: function () {
+            var style = {
+                    height: this.state.height,
+                    width: this.state.width
+                },
+                heatPoints = this.state.interactions.map(function (interaction, index) {
+                    return (<Heat x={interaction.clientX} y={interaction.clientY} key={index}/>);
+                })
+            return (
+                <svg height={this.state.height} width={this.state.width} style={style}
+                     className="heatMap">{heatPoints}</svg>
+            );
+        }
+    });
+
+    var Heat = React.createClass({
+        render: function () {
+            return (<g stroke="green" fill="white" stroke-width="5">
+                <circle cx={this.props.x} cy={this.props.y} r="15"/>
+            </g>)
         }
     })
 
-    //bootstrap
-    React.render(map, document.getElementsByTagName('body'));
-}(React))
+    React.render(
+        <HeatMap />,
+        document.getElementById('heatMapContainer')
+    );
+}(this, React))
