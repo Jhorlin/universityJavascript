19f849af95b17304dcbfc3cbd9f976ecf08f333e
Jhorlin <jhorlin@gmail.com>
2015-06-08 21:15:08 -0400

    got proxy server working

--- app.js
+++ app.js
@@ -5,17 +5,26 @@
     'use strict';
 
     var express = require('express'),
-        env = requrie('node-env-file'),
+        env = require('node-env-file'),
         path = require('path'),
+        httpProxy = require('http-proxy'),
+        proxy = httpProxy.createProxyServer({}),
         app = express();
 
     env(path.join(__dirname, '.env'));
 
-    var port = process.env.PORT;
+    var port = process.env.PORT,
+        target = process.env.TARGET;
 
     app.listen(port, function(){
         console.log('listening on port %s', port);
-    })
+    });
+
+    app.use(function(req, res, next){
+        proxy.web(req, res, {target: target}, function(err){
+            console.log(err.message);
+        })
+    });
 
 
 }())
